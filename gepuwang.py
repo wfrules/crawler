112f85681faa1683cba659439f3f0bb9b6a88d40
wfrules <37941044@qq.com>
2018-06-14 20:52:52 +0800

    44444444444

--- gepuwang.py
+++ gepuwang.py
@@ -11,14 +11,19 @@ def fetch_url(url):
     return session.get(url).content.decode('gbk')
 
 
-def getPageNum(content):
-    result = re.findall('^页<strong>\d', content)
-    print(result)
+def getPageNum(content):#获取总页码
+    result = re.findall('(?<=共 <strong>)\d*(?=</strong>页)', content)[0]  #特殊字符之间包含的表达式
+    iPageNum = int(result)
+    return iPageNum
 
 def main():
     url = 'http://www.gepuwang.net/hexianpu/list_62_1.html'   #列表页
     content = fetch_url(url)
-    getPageNum(content)
+    iPageNum = getPageNum(content)
+    iPageNum = 1
+    for i in range(iPageNum):
+        url = 'http://www.gepuwang.net/hexianpu/list_62_' + str(i+1) +'.html'
+        print(url)
 
 if __name__ == "__main__":
     main()
