1bb92d925995f2703ccf818cb076c6d89a2baa3e
wfrules <37941044@qq.com>
2018-06-14 20:14:11 +0800

    修改内容提交

--- gepuwang.py
+++ gepuwang.py
@@ -1,3 +1,24 @@
-#http://www.gepuwang.net/hexianpu/list_62_1.html
+#
 
-print("Hellodefhahahahaaaaa123")
+import requests
+import re
+import json
+import os
+
+session = requests.session()
+
+def fetch_url(url):
+    return session.get(url).content.decode('gbk')
+
+
+def getPageNum(content):
+    result = re.findall('^页<strong>\d', content)
+    print(result)
+
+def main():
+    url = 'http://www.gepuwang.net/hexianpu/list_62_1.html'   #列表页
+    content = fetch_url(url)
+    getPageNum(content)
+
+if __name__ == "__main__":
+    main()
